3929fb5cc9c4b9ce75104f1dc71129eaffdada41
Javier Lete <javier@eskura21.com>
2023-02-08 08:48:40 +0100

    Diccionario

--- Bases/src/com/ipartek/formacion/bases/ejemploholamundo/Colecciones.java
+++ Bases/src/com/ipartek/formacion/bases/ejemploholamundo/Colecciones.java
@@ -1,6 +1,8 @@
 package com.ipartek.formacion.bases.ejemploholamundo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Colecciones {
 	public static void main(String[] args) {
@@ -27,6 +29,25 @@ public class Colecciones {
 		for(String texto: textos) {
 			System.out.println(texto);
 		}
-
+		
+		Map<String, String> diccionario = new HashMap<>();
+		
+		diccionario.put("casa", "house");
+		diccionario.put("perro", "dog");
+		diccionario.put("gato", "cat");
+		
+		System.out.println(diccionario.get("perro"));
+		
+		for(Map.Entry<String, String> par: diccionario.entrySet()) {
+			System.out.printf("%s: %s\n", par.getKey(), par.getValue());
+		}
+		
+		for(String clave: diccionario.keySet()) {
+			System.out.println(clave);
+		}
+		
+		for(String valor: diccionario.values()) {
+			System.out.println(valor);
+		}
 	}
 }
