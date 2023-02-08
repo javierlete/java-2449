f4689bcbfd79a2b0d5ff88146f21a2cc3c826795
Javier Lete <javier@eskura21.com>
2023-02-08 10:04:00 +0100

    Creamos la capa de presentación

---
+++ Bases/src/com/ipartek/formacion/almacen/presentacion/AlmacenConsola.java
@@ -0,0 +1,133 @@
+package com.ipartek.formacion.almacen.presentacion;
+
+import static com.ipartek.formacion.bibliotecas.Consola.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.ipartek.formacion.almacen.accesodatos.Dao;
+import com.ipartek.formacion.almacen.accesodatos.DaoProductoMemoria;
+import com.ipartek.formacion.almacen.entidades.Producto;
+
+public class AlmacenConsola {
+	private static final int SALIR = 0;
+
+	private static Dao<Producto> dao = new DaoProductoMemoria();
+	
+	public static void main(String[] args) {
+		dao.insertar(new Producto("Producto1", new BigDecimal(1234), 5));
+		dao.insertar(new Producto("Producto2", new BigDecimal(234)));
+		dao.insertar(new Producto("Producto3", new BigDecimal(3456), 2, LocalDate.of(2030, 2, 3)));
+		
+		
+		int opcion;
+		
+		do {
+			mostrarMenu();
+			opcion = pedirOpcion();
+			ejecutarOpcion(opcion);
+		} while(opcion != SALIR);
+		
+	}
+
+	private static void mostrarMenu() {
+		pl();
+		pl("1. Listado");
+		pl("2. Buscar por id");
+		pl("3. Insertar");
+		pl("4. Modificar");
+		pl("5. Borrar");
+		pl();
+		pl("0. Salir");
+		pl();
+	}
+
+	private static int pedirOpcion() {
+		return pedirEntero("Introduce la opción deseada");
+	}
+
+	private static void ejecutarOpcion(int opcion) {
+		switch(opcion) {
+		case 0:
+			pl("Gracias por usar la aplicación");
+			break;
+		case 1:
+			listado();
+			break;
+		case 2:
+			buscarPorId();
+			break;
+		case 3:
+			insertar();
+			break;
+		case 4:
+			modificar();
+			break;
+		case 5:
+			borrar();
+			break;
+		default:
+			ple("No conozco esa opción");
+		}
+	}
+
+	private static void listado() {
+		boolean hayProductos = false;
+		
+		for(Producto producto: dao.obtenerTodos()) {
+			mostrarProductoLinea(producto);
+			hayProductos = true;
+		}
+		
+		if(!hayProductos) {
+			pl("No hay ningún producto");
+		}
+	}
+
+	private static void mostrarProductoLinea(Producto producto) {
+		pl(producto);
+	}
+
+	private static void buscarPorId() {
+		Long id = pedirLong("Introduce el id del producto");
+		
+		mostrarFichaProducto(dao.obtenerPorId(id));
+	}
+
+	private static void mostrarFichaProducto(Producto producto) {
+		pl(producto);
+	}
+
+	private static void insertar() {
+		Producto producto = pedirDatosProducto();
+		
+		dao.insertar(producto);
+	}
+
+	private static Producto pedirDatosProducto() {
+		String nombre = pedirTexto("Nombre");
+		BigDecimal precio = pedirBigDecimal("Precio");
+		Integer stock = pedirEntero("Stock");
+		LocalDate fechaCaducidad = pedirFecha("Fecha de caducidad");
+		
+		Producto producto = new Producto(nombre, precio, stock, fechaCaducidad);
+		
+		return producto;
+	}
+
+	private static void modificar() {
+		Long id = pedirLong("Id");
+		
+		Producto producto = pedirDatosProducto();
+		
+		producto.setId(id);
+		
+		dao.modificar(producto);
+	}
+
+	private static void borrar() {
+		Long id = pedirLong("Introduce el id del producto a borrar");
+		
+		dao.borrar(id);
+	}
+}
