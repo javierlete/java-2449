cfcabbd64362f9616e260130a8c57156121cc77a
Javier Lete <javier@eskura21.com>
2023-02-14 09:31:06 +0100

    DAO protegido contra datos erróneos

--- Bases/src/com/ipartek/formacion/almacen/accesodatos/DaoProductoMemoria.java
+++ Bases/src/com/ipartek/formacion/almacen/accesodatos/DaoProductoMemoria.java
@@ -21,11 +21,19 @@ public class DaoProductoMemoria implements Dao<Producto> {
 
 	@Override
 	public Producto obtenerPorId(Long id) {
+		if(id == null) {
+			throw new AccesoDatosException("No se admite buscar un id nulo");
+		}
+		
 		return productos.get(id);
 	}
 
 	@Override
 	public Producto insertar(Producto producto) {
+		if(producto.getId() != null) {
+			throw new AccesoDatosException("No se puede pasar un id para un nuevo registro");
+		}
+		
 		Long id = productos.size() > 0 ? productos.lastKey() + 1L: 1L;
 		
 		producto.setId(id);
@@ -37,6 +45,10 @@ public class DaoProductoMemoria implements Dao<Producto> {
 
 	@Override
 	public Producto modificar(Producto producto) {
+		if(obtenerPorId(producto.getId())== null) {
+			throw new AccesoDatosException("No se puede modificar un producto que no existe");
+		}
+		
 		productos.put(producto.getId(), producto);
 		
 		return producto;
@@ -44,6 +56,10 @@ public class DaoProductoMemoria implements Dao<Producto> {
 
 	@Override
 	public void borrar(Long id) {
+		if(obtenerPorId(id)== null) {
+			throw new AccesoDatosException("No se puede borrar un producto que no existe");
+		}
+		
 		productos.remove(id);
 	}
 
