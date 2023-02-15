1378a7ec232041acacc56e3d08e184f5caf67907
Javier Lete <javier@eskura21.com>
2023-02-15 08:41:25 +0100

    FabricaDao con configuración mediante clases

--- Bases/src/com/ipartek/formacion/almacen/accesodatos/FabricaDao.java
+++ Bases/src/com/ipartek/formacion/almacen/accesodatos/FabricaDao.java
@@ -5,12 +5,14 @@ import java.util.Properties;
 
 import com.ipartek.formacion.almacen.entidades.Producto;
 
+@SuppressWarnings("unchecked")
 public class FabricaDao {
 	private static final String FICHERO_CONFIGURACION = "fabrica.properties";
-	private static String tipo;
-	private static String fichero;
+	private static Dao<Producto> tipo;
 	
 	static {
+		String tipoTexto = null;
+		
 		try {
 			Properties props = new Properties();
 			InputStream ficheroProperties = FabricaDao.class.getClassLoader().getResourceAsStream(FICHERO_CONFIGURACION);
@@ -21,27 +23,31 @@ public class FabricaDao {
 			
 			props.load(ficheroProperties);
 			
-			tipo = props.getProperty("dao.tipo");
-			
-			if(tipo == null) {
+			tipoTexto = props.getProperty("dao.tipo");
+
+			if(tipoTexto == null) {
 				throw new AccesoDatosException("Necesitamos el valor dao.tipo en la configuración");
 			}
 			
-			fichero = props.getProperty("dao.fichero");
-
-			if("fichero".equals(tipo) && fichero == null) {
-				throw new AccesoDatosException("Necesitamos el valor dao.fichero en la configuración");
+			Class<?> clase = Class.forName(tipoTexto);
+			
+			String argumento = props.getProperty("dao.argumento");
+			
+			if(argumento == null) {
+				tipo = (Dao<Producto>) clase.getDeclaredConstructor().newInstance();
+			} else {
+				tipo = (Dao<Producto>) clase.getDeclaredConstructor(String.class).newInstance(argumento);
 			}
+		} catch(ClassNotFoundException e) {
+			throw new AccesoDatosException("No se ha encontrado la clase " + tipoTexto, e);
+		} catch (ClassCastException e) {
+			throw new AccesoDatosException("La clase debe implementar Dao<Producto>", e);
 		} catch (Exception e) {
 			throw new AccesoDatosException("No se ha podido leer la configuración", e);
 		}
 	}
 	
 	public static Dao<Producto> getDaoProducto() {
-		switch(tipo) {
-		case "fichero": return new DaoProductoFichero(fichero);
-		case "memoria": return new DaoProductoMemoria();
-		default: throw new AccesoDatosException("No se acepta ese tipo");
-		}
+		return tipo;
 	}
 }
