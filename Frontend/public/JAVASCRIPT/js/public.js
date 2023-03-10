12077b72bcaf432b6b0c0b18f8aed6d2b1243475
Javier Lete <javier@eskura21.com>
2023-03-10 08:30:47 +0100

    Añadir al carrito funcional

--- Frontend/public/JAVASCRIPT/js/public.js
+++ Frontend/public/JAVASCRIPT/js/public.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const URL = 'http://127.0.0.1:3000/productos/';
+let carrito = JSON.parse(localStorage.getItem('carrito'));
+
+if(!carrito) {
+    localStorage.setItem('carrito', '[]');
+}
 
 window.addEventListener('DOMContentLoaded', async function () {
     const main = document.querySelector('main');
@@ -28,6 +33,11 @@ window.addEventListener('DOMContentLoaded', async function () {
             tarjetaProducto.querySelector('.card-text').innerHTML = producto.precio;
             tarjetaProducto.querySelector('.text-muted').innerHTML = producto.id;
             tarjetaProducto.querySelector('img').src = 'https://picsum.photos/400/400?' + producto.id;
+
+            tarjetaProducto.querySelector('button').addEventListener('click', function() {
+                carrito.push(producto);
+                localStorage.setItem('carrito', JSON.stringify(carrito));
+            });
 
             main.appendChild(tarjetaProducto);
         });
