1215b4b472f1a8de40e91102277520f994817bbd
Javier Lete <javier@eskura21.com>
2023-03-14 09:48:17 +0100

    Recepción de un parámetro

--- Backend/src/main/java/com/ipartek/formacion/servlets/HolaMundo.java
+++ Backend/src/main/java/com/ipartek/formacion/servlets/HolaMundo.java
@@ -10,8 +10,15 @@ import java.io.IOException;
 @WebServlet("/hola")
 public class HolaMundo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().println("Hola a todos");
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nombre = request.getParameter("nombre");
+
+		if (nombre == null) {
+			response.getWriter().println("Hola a todos");
+		} else {
+			response.getWriter().println("Hola " + nombre);
+		}
 	}
 }
