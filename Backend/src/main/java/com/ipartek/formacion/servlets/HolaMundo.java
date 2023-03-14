476734df6bc173341b58c3c90fdfbff3347bbd06
Javier Lete <javier@eskura21.com>
2023-03-14 09:43:34 +0100

    Ejemplo de HolaMundo en servlets

---
+++ Backend/src/main/java/com/ipartek/formacion/servlets/HolaMundo.java
@@ -0,0 +1,17 @@
+package com.ipartek.formacion.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hola")
+public class HolaMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Hola a todos");
+	}
+}
