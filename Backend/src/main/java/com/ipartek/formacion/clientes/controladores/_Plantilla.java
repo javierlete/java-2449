b8fdf98fde2b338d7b38657021c1257971bb2854
Javier Lete <javier@eskura21.com>
2023-03-23 08:40:40 +0100

    Plantillas para servlet y JSP

---
+++ Backend/src/main/java/com/ipartek/formacion/clientes/controladores/_Plantilla.java
@@ -0,0 +1,26 @@
+package com.ipartek.formacion.clientes.controladores;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import static com.ipartek.formacion.clientes.controladores.Globales.*;
+
+@SuppressWarnings("unused")
+@WebServlet("")
+public class _Plantilla extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ Backend/src/main/webapp/WEB-INF/vistas/_plantilla.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/WEB-INF/vistas/includes/cabecera.jsp"%>
+
+
+
+<%@ include file="/WEB-INF/vistas/includes/pie.jsp"%>
