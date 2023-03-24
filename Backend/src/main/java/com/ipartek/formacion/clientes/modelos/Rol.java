4ce7f5327ddc286dc24e3eef253b269d49f92caf
Javier Lete <javier@eskura21.com>
2023-03-24 09:23:10 +0100

    Nueva clase Rol

---
+++ Backend/src/main/java/com/ipartek/formacion/clientes/modelos/Rol.java
@@ -0,0 +1,76 @@
+package com.ipartek.formacion.clientes.modelos;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Rol {
+	private Long id;
+	private String nombre;
+	private String descripcion;
+
+	private Set<Usuario> usuarios = new HashSet<>();
+
+	public Rol(Long id, String nombre, String descripcion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Set<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(Set<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(descripcion, id, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rol other = (Rol) obj;
+		return Objects.equals(descripcion, other.descripcion) && Objects.equals(id, other.id)
+				&& Objects.equals(nombre, other.nombre);
+	}
+
+	@Override
+	public String toString() {
+		return "Rol [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + "]";
+	}
+
+}
