6e744f5669691c410427eb0a77bd63e87b33b37c
Javier Lete <javier@eskura21.com>
2023-03-27 10:33:20 +0200

    Versión de MySQL

---
+++ Backend/src/main/java/com/ipartek/formacion/clientes/accesodatos/DaoClienteMySQL.java
@@ -0,0 +1,158 @@
+package com.ipartek.formacion.clientes.accesodatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ipartek.formacion.clientes.modelos.Cliente;
+
+public class DaoClienteMySQL implements DaoCliente {
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new AccesoDatosException("No se ha encontrado el driver de MySQL", e);
+		}
+	}
+	
+	private final String URL;
+
+	private static final String SQL_SELECT = "SELECT * FROM clientes";
+	private static final String SQL_SELECT_ID = "SELECT * FROM clientes WHERE id=?";
+
+	private static final String SQL_INSERT = "INSERT INTO clientes (nombre, nif, telefono, email, fecha_nacimiento) VALUES (?,?,?,?,?)";
+
+	private static final String SQL_UPDATE = "UPDATE clientes SET nombre=?, nif=?, telefono=?, email=?, fecha_nacimiento=? WHERE id=?";
+
+	private static final String SQL_DELETE = "DELETE FROM clientes WHERE id=?";
+
+	public DaoClienteMySQL(String baseDeDatos) {
+		URL = "jdbc:mysql://" + baseDeDatos;
+	}
+
+	private Connection obtenerConexion() {
+		try {
+			return DriverManager.getConnection(URL);
+		} catch (SQLException e) {
+			throw new AccesoDatosException("No se ha podido conectar a la base de datos", e);
+		}
+	}
+
+	@Override
+	public Iterable<Cliente> obtenerTodos() {
+		try (Connection con = obtenerConexion();
+				PreparedStatement pst = con.prepareStatement(SQL_SELECT);
+				ResultSet rs = pst.executeQuery()) {
+			List<Cliente> clientes = new ArrayList<>();
+
+			Cliente cliente;
+
+			while (rs.next()) {
+				cliente = new Cliente(rs.getLong("id"), rs.getString("nombre"), rs.getString("nif"),
+						rs.getString("telefono"), rs.getString("email"), rs.getString("fecha_nacimiento"));
+
+				clientes.add(cliente);
+			}
+
+			return clientes;
+		} catch (SQLException e) {
+			throw new AccesoDatosException("No se han podido obtener todos los registros", e);
+		}
+	}
+
+	@Override
+	public Cliente obtenerPorId(Long id) {
+		try (Connection con = obtenerConexion(); PreparedStatement pst = con.prepareStatement(SQL_SELECT_ID);) {
+
+			pst.setLong(1, id);
+
+			ResultSet rs = pst.executeQuery();
+
+			Cliente cliente = null;
+
+			if (rs.next()) {
+				cliente = new Cliente(rs.getLong("id"), rs.getString("nombre"), rs.getString("nif"),
+						rs.getString("telefono"), rs.getString("email"), rs.getString("fecha_nacimiento"));
+			}
+
+			return cliente;
+		} catch (SQLException e) {
+			throw new AccesoDatosException("No se ha podido obtener el registro id=" + id, e);
+		}
+	}
+
+	@Override
+	public Cliente insertar(Cliente cliente) {
+		try (Connection con = obtenerConexion(); PreparedStatement pst = con.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);) {
+
+			pst.setString(1, cliente.getNombre());
+			pst.setString(2, cliente.getNif());
+			pst.setString(3, cliente.getTelefono());
+			pst.setString(4, cliente.getEmail());
+			pst.setString(5, cliente.getFechaNacimientoTexto());
+
+			int modificados = pst.executeUpdate();
+
+			if (modificados != 1) {
+				throw new AccesoDatosException("Se ha insertado 0 o más de un cliente");
+			}
+			
+			ResultSet rs = pst.getGeneratedKeys();
+			
+			rs.next();
+			
+			Long id = rs.getLong(1);
+			
+			cliente.setId(id);
+
+			return cliente;
+		} catch (SQLException e) {
+			throw new AccesoDatosException("No se ha podido insertar el registro", e);
+		}
+	}
+
+	@Override
+	public Cliente modificar(Cliente cliente) {
+		try (Connection con = obtenerConexion(); PreparedStatement pst = con.prepareStatement(SQL_UPDATE);) {
+
+			pst.setString(1, cliente.getNombre());
+			pst.setString(2, cliente.getNif());
+			pst.setString(3, cliente.getTelefono());
+			pst.setString(4, cliente.getEmail());
+			pst.setString(5, cliente.getFechaNacimientoTexto());
+			
+			pst.setLong(6, cliente.getId());
+
+			int modificados = pst.executeUpdate();
+
+			if (modificados != 1) {
+				throw new AccesoDatosException("Se ha modificado 0 o más de un cliente");
+			}
+
+			return cliente;
+		} catch (SQLException e) {
+			throw new AccesoDatosException("No se ha podido modificar el registro", e);
+		}
+	}
+
+	@Override
+	public void borrar(Long id) {
+		try (Connection con = obtenerConexion(); PreparedStatement pst = con.prepareStatement(SQL_DELETE);) {
+
+			pst.setLong(1, id);
+
+			int modificados = pst.executeUpdate();
+
+			if (modificados != 1) {
+				throw new AccesoDatosException("Se ha borrado 0 o más de un cliente");
+			}
+		} catch (SQLException e) {
+			throw new AccesoDatosException("No se ha podido borrar el registro", e);
+		}
+	}
+}
--- Backend/src/main/java/com/ipartek/formacion/clientes/controladores/Globales.java
+++ Backend/src/main/java/com/ipartek/formacion/clientes/controladores/Globales.java
@@ -1,17 +1,24 @@
 package com.ipartek.formacion.clientes.controladores;
 
 import com.ipartek.formacion.clientes.accesodatos.DaoCliente;
-import com.ipartek.formacion.clientes.accesodatos.DaoClienteSQLite;
+import com.ipartek.formacion.clientes.accesodatos.DaoClienteMySQL;
 import com.ipartek.formacion.clientes.accesodatos.DaoRol;
-import com.ipartek.formacion.clientes.accesodatos.DaoRolSQLite;
+import com.ipartek.formacion.clientes.accesodatos.DaoRolMySQL;
 import com.ipartek.formacion.clientes.accesodatos.DaoUsuario;
-import com.ipartek.formacion.clientes.accesodatos.DaoUsuarioSQLite;
+import com.ipartek.formacion.clientes.accesodatos.DaoUsuarioMySQL;
 
 public class Globales {
-	private static final String SQLITE_DB = "/Users/javierlete/git/java-2449/Backend/sql/clientes.db";
+//	private static final String SQLITE_DB = "/Users/javierlete/git/java-2449/Backend/sql/clientes.db";
 
+	private static final String BASE_DE_DATOS = "root:admin@localhost:3306/tienda2449";
+	
 	public static final String VISTAS = "/WEB-INF/vistas";
-	public static final DaoCliente DAO_CLIENTES = new DaoClienteSQLite(SQLITE_DB);
-	public static final DaoUsuario DAO_USUARIO = new DaoUsuarioSQLite(SQLITE_DB);
-	public static final DaoRol DAO_ROL = new DaoRolSQLite(SQLITE_DB);
+	
+	public static final DaoCliente DAO_CLIENTES = new DaoClienteMySQL(BASE_DE_DATOS);
+	public static final DaoUsuario DAO_USUARIO = new DaoUsuarioMySQL(BASE_DE_DATOS);
+	public static final DaoRol DAO_ROL = new DaoRolMySQL(BASE_DE_DATOS);
+	
+//	public static final DaoCliente DAO_CLIENTES = new DaoClienteSQLite(SQLITE_DB);
+//	public static final DaoUsuario DAO_USUARIO = new DaoUsuarioSQLite(SQLITE_DB);
+//	public static final DaoRol DAO_ROL = new DaoRolSQLite(SQLITE_DB);
 }
