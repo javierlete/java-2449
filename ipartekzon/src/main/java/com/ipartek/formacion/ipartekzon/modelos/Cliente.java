7776e3ef60878bd00ece6563325125376eefa7cc
Javier Lete <javier@eskura21.com>
2023-03-31 08:54:58 +0200

    Configuramos Cliente como una entidad

--- ipartekzon/src/main/java/com/ipartek/formacion/ipartekzon/modelos/Cliente.java
+++ ipartekzon/src/main/java/com/ipartek/formacion/ipartekzon/modelos/Cliente.java
@@ -1,12 +1,22 @@
 package com.ipartek.formacion.ipartekzon.modelos;
 
-import lombok.Builder;
-import lombok.Data;
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name = "clientes")
 @Data
 @Builder
 public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nif;
 	private String nombre;
