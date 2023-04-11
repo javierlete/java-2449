dd871dec950f06b71c6e150ad1ffaa3acbf3e3c0
Javier Lete <javier@eskura21.com>
2023-04-11 09:19:16 +0200

    springzon: Data JPA, Thymeleaf, Validation, Web, DevTools, MySQL, Lombok

---
+++ springzon/src/main/java/com/ipartek/formacion/springzon/SpringzonApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springzon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringzonApplication.class, args);
+	}
+
+}
---
+++ springzon/src/test/java/com/ipartek/formacion/springzon/SpringzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
