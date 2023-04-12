7de1ca78331b5e9e28e13fc87a831b2746d8f525
Javier Lete <javier@eskura21.com>
2023-04-12 09:38:45 +0200

    Mantenimiento completo de clientes

--- springzon/src/main/java/com/ipartek/formacion/springzon/controladores/ClientesController.java
+++ springzon/src/main/java/com/ipartek/formacion/springzon/controladores/ClientesController.java
@@ -3,9 +3,14 @@ package com.ipartek.formacion.springzon.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ipartek.formacion.springzon.entidades.Cliente;
 import com.ipartek.formacion.springzon.servicios.ClienteService;
 
 @Controller
@@ -19,5 +24,39 @@ public class ClientesController {
 		modelo.addAttribute("clientes", servicio.listado());
 		
 		return "admin/clientes";
+	}
+	
+	@GetMapping("/anadir")
+	public String anadir(Cliente cliente) {
+		return "admin/cliente";
+	}
+
+	@GetMapping("/editar/{id}")
+	public String editar(@PathVariable Long id, Model modelo) {
+		modelo.addAttribute("cliente", servicio.consultarPorId(id));
+		
+		return "admin/cliente";
+	}
+	
+	@GetMapping("/borrar/{id}")
+	public String editar(@PathVariable Long id) {
+		servicio.eliminar(id);
+		
+		return "redirect:/admin/clientes";
+	}
+	
+	@PostMapping
+	public String guardar(@Validated Cliente cliente, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "admin/cliente";
+		}
+		
+		if(cliente.getId() == null) {
+			servicio.cambiar(cliente);
+		} else {
+			servicio.nuevo(cliente);
+		}
+		
+		return "redirect:/admin/clientes";
 	}
 }
