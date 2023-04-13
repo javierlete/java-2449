50efbcb753c599556700cbb1b190b5960de8e95f
Javier Lete <javier@eskura21.com>
2023-04-13 10:26:39 +0200

    Configuración de autenticación por JDBC con MySQL

--- springzon/src/main/java/com/ipartek/formacion/springzon/configuraciones/WebSecurityConfig.java
+++ springzon/src/main/java/com/ipartek/formacion/springzon/configuraciones/WebSecurityConfig.java
@@ -1,13 +1,15 @@
 package com.ipartek.formacion.springzon.configuraciones;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,21 +32,19 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
-	@Bean
-	UserDetailsService userDetailsService() {
-		UserDetails admin =
-			 User.withDefaultPasswordEncoder()
-				.username("javier")
-				.password("lete")
-				.roles("ADMIN")
-				.build();
-		UserDetails user = 
-			User.withDefaultPasswordEncoder()
-				.username("pepe")
-				.password("perez")
-				.roles("USER")
-				.build();
+	// https://www.baeldung.com/spring-security-jdbc-authentication
+	
+	@Autowired
+	private DataSource dataSource;
 
-		return new InMemoryUserDetailsManager(admin, user);
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication().dataSource(dataSource);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 }
