f278f5576394c905c87c584b1af3ea66b93e37ad
Javier Lete <javier@eskura21.com>
2023-04-13 10:26:32 +0200

    Base de datos para JDBC authentication

---
+++ springzon/sql/jdbcAuthentication.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  enabled TINYINT NOT NULL DEFAULT 1,
+  PRIMARY KEY (username)
+);
+  
+CREATE TABLE authorities (
+  username VARCHAR(50) NOT NULL,
+  authority VARCHAR(50) NOT NULL,
+  FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE UNIQUE INDEX ix_auth_username
+  on authorities (username,authority);
+  
+-- User user/pass
+INSERT INTO users (username, password, enabled)
+  values ('javier',
+    '$2a$12$/AY0/zbOo3CKTZE6.b41TOcHyP.VMLCHVbsfiW3KnVLcYe3pz/VLC',
+    1),
+    ('pepe',
+    '$2a$12$i2vbn0K2ttmBOsUFaH98TOAyHb8HeyPABEq.IceE6TvlumEOJwwb.',
+    1);
+
+INSERT INTO authorities (username, authority)
+  values ('javier', 'ROLE_ADMIN'), ('pepe', 'ROLE_USER');
