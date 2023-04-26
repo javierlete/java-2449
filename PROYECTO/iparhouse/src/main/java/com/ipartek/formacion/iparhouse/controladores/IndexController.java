d7bd760b6af61c7cc4ec24b8de00f2b62eb2b65a
Javier Lete <javier@eskura21.com>
2023-04-26 13:22:11 +0200

    Mostrar detalle

--- PROYECTO/iparhouse/src/main/java/com/ipartek/formacion/iparhouse/controladores/IndexController.java
+++ PROYECTO/iparhouse/src/main/java/com/ipartek/formacion/iparhouse/controladores/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,21 +19,28 @@ import lombok.extern.java.Log;
 public class IndexController {
 	@Autowired
 	private PublicoService servicio;
-	
+
 	@GetMapping
 	public String index(Model modelo, Busqueda busqueda) {
 		modelo.addAttribute("inmuebles", servicio.listado());
-		modelo.addAttribute("inmueble", servicio.detalle(1L));
-		
+
 		return "index";
 	}
-	
+
 	@PostMapping
 	public String busqueda(Model modelo, Busqueda busqueda) {
 		log.info(busqueda.toString());
-		
+
 		modelo.addAttribute("inmuebles", servicio.listado(busqueda));
-		
+
+		return "index";
+	}
+
+	@GetMapping("/detalle/{id}")
+	public String detalle(@PathVariable Long id, Model modelo, Busqueda busqueda) {
+		modelo.addAttribute("inmueble", servicio.detalle(id));
+		modelo.addAttribute("inmuebles", servicio.listado());
+
 		return "index";
 	}
 }
