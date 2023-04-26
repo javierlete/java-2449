4ac6bff3f4af110aef35e54d30107d2b2147b5e1
Javier Lete <javier@eskura21.com>
2023-04-26 12:28:48 +0200

    Servicios

--- PROYECTO/iparhouse/src/main/java/com/ipartek/formacion/iparhouse/entidades/Servicio.java
+++ PROYECTO/iparhouse/src/main/java/com/ipartek/formacion/iparhouse/entidades/Servicio.java
@@ -1,17 +1,13 @@
 package com.ipartek.formacion.iparhouse.entidades;
 
-import java.math.BigDecimal;
-import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,28 +19,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "inmuebles")
-public class Inmueble {
+@Table(name = "servicios")
+public class Servicio {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
-	@NotNull
-	@Size(min = 5, max = 255)
-	private String nombre;
-
-	@NotNull
-	@Size(min = 5, max = 255)
-	private String direccion;
-
-	@NotNull
-	@Pattern(regexp = "(Alquiler|Venta)")
-	private String tipo; // Alquiler o Venta
-
-	@NotNull
-	@Min(0)
-	private BigDecimal precio;
 	
-	@OneToMany(mappedBy = "inmueble")
-	private Set<Servicio> servicios;
+	@NotNull
+	@Size(min = 2, max = 20)
+	private String nombre;
+	
+	@NotNull
+	@Min(1)
+	@Builder.Default
+	private Integer cantidad = 1;
+	
+	@NotNull
+	@ManyToOne
+	private Inmueble inmueble;
 }
