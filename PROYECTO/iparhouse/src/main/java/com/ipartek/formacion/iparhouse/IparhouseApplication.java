ca4cbe2e8c572a17096f90f6958971a3daa647cf
Javier Lete <javier@eskura21.com>
2023-04-26 09:46:18 +0200

    Iparhouse inicio

---
+++ PROYECTO/iparhouse/src/main/java/com/ipartek/formacion/iparhouse/IparhouseApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.iparhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IparhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IparhouseApplication.class, args);
+	}
+
+}
---
+++ PROYECTO/iparhouse/src/test/java/com/ipartek/formacion/iparhouse/IparhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.iparhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IparhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
