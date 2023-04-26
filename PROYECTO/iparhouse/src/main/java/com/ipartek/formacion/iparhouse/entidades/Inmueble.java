2826f79e43730342dedcff12e9402efc7abcf903
Javier Lete <javier@eskura21.com>
2023-04-26 12:19:25 +0200

    Convertimos Inmueble en una entidad con persistencia y validaciones

--- PROYECTO/iparhouse/src/main/java/com/ipartek/formacion/iparhouse/entidades/Inmueble.java
+++ PROYECTO/iparhouse/src/main/java/com/ipartek/formacion/iparhouse/entidades/Inmueble.java
@@ -2,6 +2,15 @@ package com.ipartek.formacion.iparhouse.entidades;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +20,26 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "inmuebles")
 public class Inmueble {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NotNull
+	@Size(min = 5, max = 255)
 	private String nombre;
+
+	@NotNull
+	@Size(min = 5, max = 255)
 	private String direccion;
+
+	@NotNull
+	@Pattern(regexp = "(Alquiler|Venta)")
 	private String tipo; // Alquiler o Venta
+
+	@NotNull
+	@Min(0)
 	private BigDecimal precio;
 }
